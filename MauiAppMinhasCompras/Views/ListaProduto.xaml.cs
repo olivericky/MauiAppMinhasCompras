ea49f4a353cc839c0b1c6ea7426d75389a7b2796
olivericky <ricardo_luiz16@hotmail.com>
2026-03-22 15:49:17 -0300

    Utilização de SearchBar e ObservableCollection.

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,13 +1,28 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
-	}
 
-	private void ToolbarItem_Clicked(object sender, EventArgs e)
+		lst_produtos.ItemsSource = lista;
+    }
+
+	protected async override void OnAppearing()
+	{
+		List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(i => lista.Add(i));
+    }
+
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
 	{
 		try
 		{
@@ -18,4 +33,28 @@ public partial class ListaProduto : ContentPage
         }
     }
 
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:C}";
+
+		DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
